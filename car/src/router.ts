7e1c7c1b6ba542fc662d17bfd96dd0ad88464f6b
wangshiyu <1102404281@qq.com>
2019-07-17 11:40:38 +0800

    修改

--- car/src/router.ts
+++ car/src/router.ts
@@ -29,7 +29,7 @@ export default new Router({
     {
       path: '/quotation',
       name: 'quotation',
-      component: () => import(/* webpackChunkName: "about" */ './views/quotation.vue')
+      component: () => import(/* webpackChunkName:"about" */ './views/quotation.vue')
     }
   ]
 })
